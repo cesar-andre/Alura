7e0d8e9ab19af1e185950565c4e6ff941a454e1f
Cesar André de Paula <slimcesar@gmail.com>
2017-01-19 11:25:26 -0200

    Java II - Aula 5

--- Java II/Eclipse/fj11/src/fj11/ContaPoupanca.java
+++ Java II/Eclipse/fj11/src/fj11/ContaPoupanca.java
@@ -5,7 +5,9 @@ public class ContaPoupanca extends Conta {
 	public ContaPoupanca(String nome, int agencia, double saldo) {
 		super(nome, agencia, saldo);
 	}
-	
-	
 
+	@Override
+	public void atualiza(double taxa) {
+		this.saldo = this.saldo + (this.saldo * taxa);
+	}
 }
